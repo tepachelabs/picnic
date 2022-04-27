3702291ac4e1eeb8b7cb057a3e1c1882fbcdffd7
tonymtz <atony.mtz@gmail.com>
2022-04-27 07:22:07 -0500

    Admin can't be accessed in prod

--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -1,8 +1,13 @@
 import { Outlet } from '@remix-run/react'
+import { redirect } from '@remix-run/node'
 
 import { AdminHeader } from '~/shared/admin-header.component'
 
-export default function ScheduleRoute () {
+export default function AdminRoute () {
+  if (process.env.NODE_ENV !== 'development') {
+    return redirect('/')
+  }
+
   return (
     <section>
       <AdminHeader />
